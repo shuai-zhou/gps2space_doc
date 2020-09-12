8be03e7226f5e7a44c2a6af6dbfc0a3ca5bd603f
Iorent <zhsh0312@yeah.net>
2020-09-12 19:51:13 -0400

    newly built files

--- source/conf.py
+++ source/conf.py
@@ -27,8 +27,7 @@ author = 'Shuai Zhou'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['nbsphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -36,7 +35,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 
 # -- Options for HTML output -------------------------------------------------

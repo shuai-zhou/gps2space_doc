e6073bd611689c9152591a10ac73928d376ab455
Iorent <zhsh0312@yeah.net>
2020-09-12 19:57:29 -0400

    change conf.py

--- source/conf.py
+++ source/conf.py
@@ -49,3 +49,11 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- error: contents.rst not found -------------------------------------------
+# If you have this error master file .../.../contents.rst not found
+# Refer to:
+# https://stackoverflow.com/questions/56336234/build-fail-sphinx-error-contents-rst-not-found
+master_doc = 'index'
+
+
